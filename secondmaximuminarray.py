0c42e28310a4e365ef71069ae4cc405282876f63
ASHUTOSH KUMBHAR <ashutoshkumbhar27@gmail.com>
2021-10-05 11:42:42 +0530

    New

--- secondmaximuminarray.py
+++ secondmaximuminarray.py
@@ -1,1 +1,18 @@
 print("Hello World")
+arr = []
+n = int(input("Enter Number of Elements in array"))
+for i in range(n):
+    no = int(input("Enter Number"))
+    arr.append(no)
+
+first = 0
+second = 0
+
+for x in arr:
+    if(first<x):
+        second = first
+        first = x
+    elif(second<x and x!=first):
+        second = x
+print('Second Largest Element in Array is',second)
+
