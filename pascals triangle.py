b3f25beac682dc44ee4dce1e59d542ffa2fd6e44
ASHUTOSH KUMBHAR <ashutoshkumbhar27@gmail.com>
2021-10-05 11:24:54 +0530

    New

--- pascals triangle.py
+++ pascals triangle.py
@@ -12,4 +12,22 @@ while (flag != '0'):
             print(" " * (n - x), end="")
             print(" ".join(map(str, str(11 ** x))))
             # Two Sum
+    if (choice == "B"):
+        print("-------Welcome to Two Sum Program--------")
+        arr = []
+        count = int(input("Enter Number of elements in Array"))
+        for x in range(count):
+            no = int(input("Enter Number"))
+            arr.append(no)
+        print("Array is :", arr)
+        target = int(input('Enter Target'))
+        hashmap = {}
+        for x, y in enumerate(arr):
+            diff = target - y
+            if (diff in hashmap):
+                print('Indices are', [hashmap[diff], x])
+            hashmap[y] = x
+    if (choice == "0"):
+        flag = "0"
+
     
